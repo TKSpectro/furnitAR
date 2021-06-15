7eb6fadce7f26b7b3338809e35d2f5d4cc9f1107
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2021-06-15 15:48:46 +0200

    add a script for getting the boundary -> only works if you build the project

---
+++ Assets/BoundaryTest.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryTest : MonoBehaviour
+{
+    [TextArea]
+    public string Notes = "This script just works when the project is being build on the quest-> https://forum.unity.com/threads/can-we-reuse-user-s-vr-boundaries.818331/#post-5423601";
+
+    public GameObject wallMarker;
+    bool isBoundaryFinished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Safe the type for easier further use -> OuterBoundary are 256 point in counterclockwise order from the boundary
+        // PlayArea is the best possible Reactangle from all the points
+        OVRBoundary.BoundaryType type = OVRBoundary.BoundaryType.OuterBoundary;
+
+        // If a boundary is found and it wasnt already drawn
+        if (OVRManager.boundary.GetConfigured() && !isBoundaryFinished)
+        {
+            // Get the boundary and instantiate cubes on every position
+            Vector3[] boundary = OVRManager.boundary.GetGeometry(type);
+            foreach (Vector3 pos in boundary)
+            {
+                Instantiate(wallMarker, pos, Quaternion.identity);
+            }
+
+            isBoundaryFinished = true;
+        }
+
+    }
+}
