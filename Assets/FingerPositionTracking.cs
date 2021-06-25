3f9b9e422a6a220300dec000940bc2d0a3a567a9
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2021-06-25 17:30:14 +0200

    add fingertracking

---
+++ Assets/FingerPositionTracking.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OculusSampleFramework;
+
+public class FingerPositionTracking : MonoBehaviour
+{
+    OVRHand leftHand;
+    OVRHand rightHand;
+
+    public GameObject fingerTip;
+    public GameObject sphere;
+
+    bool draw = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftHand = GameObject.FindObjectOfType<HandsManager>().LeftHand;
+        rightHand = GameObject.FindObjectOfType<HandsManager>().RightHand;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightHand.IsTracked && draw)
+        {
+            Vector3 pos = rightHand.gameObject.GetComponent<OVRSkeleton>().Bones[20].Transform.position;
+            //Debug.Log(pos);
+            fingerTip.transform.position = pos;
+            Instantiate(sphere, pos, Quaternion.identity, null);
+        }
+
+    }
+
+    public void StartDraw()
+    {
+        draw = true;
+    }
+
+    public void StopDraw()
+    {
+        draw = false;
+    }
+}
