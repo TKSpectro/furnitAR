1e0fe15262b7a8a14baf5e93893de30063e2a876
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2021-07-09 09:44:09 +0200

    remove boughtItems.csv from git

--- Assets/Scripts/HandMenu/BuySelectedItems.cs
+++ Assets/Scripts/HandMenu/BuySelectedItems.cs
@@ -16,24 +16,24 @@ public class BuySelectedItems : MonoBehaviour
     {
         string filePath = getPath();
 
-        //This is the writer, it writes to the filepath
-        StreamWriter writer = new StreamWriter(filePath);
-
-        //This is writing the line of the type, name, damage... etc... (I set these)
-        writer.WriteLine("Type, Name, Price");
-        //This loops through everything in the inventory and sets the file to these.
-        foreach (GameObject item in items)
+        // Create a writer and use AppendText so if the file doesnt exist already it gets created
+        using (StreamWriter writer = File.AppendText(filePath))
         {
-            writer.WriteLine(item.GetType().ToString() +
-                            "," + item.name +
-                            "," + item.GetComponent<FurnitureAttributes>().price.ToString());
+            writer.WriteLine("Type, Name, Price");
+            // This loops through everything in the item list and writes given parameters
+            foreach (GameObject item in items)
+            {
+                writer.WriteLine(item.GetType().ToString() +
+                                "," + item.name +
+                                "," + item.GetComponent<FurnitureAttributes>().price.ToString());
+            }
+
+            writer.WriteLine("ShoppinCartValue: " + GetComponent<MenuHelper>().CalculateShoppingCartValue(items).ToString());
+
+            writer.Flush();
+            // This closes the file
+            writer.Close();
         }
-
-        writer.WriteLine("ShoppinCartValue: " + GetComponent<MenuHelper>().CalculateShoppingCartValue(items).ToString());
-
-        writer.Flush();
-        //This closes the file
-        writer.Close();
     }
 
     private string getPath()
