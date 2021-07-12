f9a1b47e30bd604119fe8dd0bbe382cd65e6c038
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2021-07-12 14:26:01 +0200

    fixup hand menu -> to get scroll working set the velocityType type on the scrolling object collection to either the same or something else. WHILE IN PLAY MODE

--- Assets/Scripts/HandMenu/ItemList.cs
+++ Assets/Scripts/HandMenu/ItemList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
 
 [RequireComponent(typeof(MenuHelper))]
 public class ItemList : MonoBehaviour
@@ -22,8 +23,18 @@ public class ItemList : MonoBehaviour
         }
 
         List<GameObject> placedItems = gameObject.GetComponent<MenuHelper>().FindPlacedItems();
+
         foreach (GameObject item in placedItems)
         {
+            // Add MeshRenderers to the Clipping Bounds Renderer Array
+            //GameObject clippingBoundsObject = FindInActiveObjectByName("Clipping Bounds");
+            //ClippingBox clippingBox = clippingBoundsObject.GetComponent<ClippingBox>();
+            //MeshRenderer[] meshRenderers = item.GetComponentsInChildren<MeshRenderer>();
+            //for (int i = 0; i < meshRenderers.Length; i++)
+            //{
+            //    clippingBox.AddRenderer(meshRenderers[i]);
+            //}
+
             // Instantiate the itemPrefab
             GameObject newItem = Instantiate(itemPrefab, itemList.transform);
 
@@ -31,6 +42,16 @@ public class ItemList : MonoBehaviour
             newItem.transform.Find("Price").GetComponent<TextMeshPro>().text = item.GetComponent<FurnitureAttributes>().price.ToString() + ".00€";
             // Create a minified version of the actual object as a icon
             GameObject model = Instantiate(item, newItem.transform.Find("Model"));
+            // Also we have to remove every boxCollider for the scrollingObjectCollection to work correctly.
+            // Appearently it also looks into the children and searches for their colliders 
+            foreach (var collider in model.GetComponentsInChildren<BoxCollider>())
+            {
+                Destroy(collider);
+            }
+            foreach (var collider in model.GetComponentsInChildren<MeshCollider>())
+            {
+                Destroy(collider);
+            }
 
             // Reset position and rotation because we duplicate the actual model in the scene
             // Dont reset rotation because the furniture prefabs are rotated randomly, so we need to keep the rotations
@@ -50,10 +71,23 @@ public class ItemList : MonoBehaviour
     // Appearently we need to call UpdateCollection on a GridObjectCollection async even if its not documented to be async
     private IEnumerator CustomUpdateCollection(GridObjectCollection gridObjectCollection)
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(0.1f);
-            gridObjectCollection.UpdateCollection();
-        }
+        yield return new WaitForSeconds(0.01f);
+        gridObjectCollection.UpdateCollection();
     }
+
+    //GameObject FindInActiveObjectByName(string name)
+    //{
+    //    Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+    //    for (int i = 0; i < objs.Length; i++)
+    //    {
+    //        if (objs[i].hideFlags == HideFlags.None)
+    //        {
+    //            if (objs[i].name == name)
+    //            {
+    //                return objs[i].gameObject;
+    //            }
+    //        }
+    //    }
+    //    return null;
+    //}
 }
